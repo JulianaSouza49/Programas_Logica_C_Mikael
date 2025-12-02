eedf7541774c74160dd35ef7234129743739eaa1
JulianaSouza49 <lj.m.msouza@gmail.com>
2025-12-02 16:39:34 -0300

    Add files via upload

---
+++ subtrairdoisnumeros.c
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+#include<studio.h>
+int main(){
+int x;
+int y;
+int subtracao;
+printf("Digite o valor de x: ");
+scanf("%d",&x);
+printf("Digite  o valor de y: ");
+scanf("%d",&y);
+subtracao = x - y;
+printf("A subtracao eh: %d",subtracao)
